9bbf0969450c391155a21bf8fe6f773bb75a1d63
PhilippL96 <phil.liesenfeld@gmail.com>
2024-07-22 12:20:18 +0200

    Visualisierungen optmiert

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from visuals import create_histplot, create_lineplot
 
 
 st.header('Bewertungsanalyse')
-col1, col2 = st.columns(2)
+
+col1, col2 = st.columns([5, 2])
 
 company = st.sidebar.text_input('Die Bewertungen welches/welcher Unternehmens/App/Website willst du analysieren?', value='data-science-institute.de', help='Bitte achte darauf, den korrekten Namen der zugehörigen Website anzugeben.')
 pagecount = st.sidebar.slider('Wie viele Seiten Bewertungen willst du scrapen?', min_value=1, max_value=100, value=10, help='Je mehr Seiten du willst, desto präziser ist die Analyse - allerdings dauert sie dann auch länger.')
@@ -15,7 +16,10 @@ else:
     eng = False
 
 df = scrape_reviews(company, pagecount, eng)
+hist = create_histplot(df)
+line = create_lineplot(df)
 
 with col1:
+
     st.pyplot(create_histplot(df))
     st.pyplot(create_lineplot(df))
