0ac9f258d99f5cbe3f7163366b06131aa4bb70f4
PhilippL96 <phil.liesenfeld@gmail.com>
2024-07-23 11:36:36 +0200

    Wording

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.header('Bewertungsanalyse')
 col1, col2 = st.columns([5, 2])
 
 company = st.sidebar.text_input('Die Bewertungen welches/welcher Unternehmens/App/Website willst du analysieren?', value='data-science-institute.de', help='Bitte achte darauf, den korrekten Namen der zugehörigen Website anzugeben.')
-pagecount = st.sidebar.slider('Wie viele Seiten Bewertungen möchtest du scrapen?', min_value=1, max_value=100, value=10, help='Je mehr Seiten du scrapest, desto präziser ist die Analyse - allerdings dauert sie dann auch länger.')
+pagecount = st.sidebar.slider('Wie viele Seiten Bewertungen möchtest du scrapen?', min_value=1, max_value=500, value=10, help='Je mehr Seiten du scrapest, desto präziser ist die Analyse - allerdings dauert sie dann auch länger.')
 english = st.sidebar.selectbox('Möchtest du englischsprachige Bewertungen analysieren?', ['Ja', 'Nein'], index=1)
 if english == 'Ja':
     eng = True
