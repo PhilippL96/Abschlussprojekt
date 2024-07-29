f7370d6006a797fb3c5ad99fdefef62beb90dc41
PhilippL96 <phil.liesenfeld@gmail.com>
2024-07-29 21:36:12 +0200

    ML wieder ermöglicht

--- app.py
+++ app.py
@@ -9,21 +9,19 @@ st.sidebar.title('Parameter bestimmen')
 
 company = st.sidebar.text_input('Die Bewertungen welches/welcher Unternehmens/App/Website willst du analysieren?', value='data-science-institute.de', help='Bitte achte darauf, den korrekten Namen der zugehörigen Website anzugeben.')
 
-
-
+reset_pagecount = st.sidebar.button('Alle vorhandenen Seiten scrapen')
 if 'reset_pagecount' not in st.session_state:
     st.session_state.reset_pagecount = False
 
-def toggle_reset_pagecount():
-    st.session_state.reset_pagecount = not st.session_state.reset_pagecount
+if reset_pagecount:
+    st.session_state.reset_pagecount = True
+    st.session_state.pagecount = None
 
-reset_pagecount_button = st.sidebar.button('Alle vorhandenen Seiten scrapen', on_click=toggle_reset_pagecount)
-
-if st.session_state.reset_pagecount:
-    pagecount = None
-else:
+if not st.session_state.reset_pagecount:
     pagecount = st.sidebar.slider('Wie viele Seiten Bewertungen möchtest du scrapen?', min_value=1, max_value=500, value=10, help='Je mehr Seiten du scrapest, desto präziser ist die Analyse - allerdings dauert sie dann auch länger.')
-    
+else:
+    pagecount = None
+
 english = st.sidebar.selectbox('Möchtest du englischsprachige Bewertungen analysieren?', ['Ja', 'Nein'], index=1)
 eng = english == 'Ja'
 
@@ -59,27 +57,32 @@ if st.session_state.scrape_button_clicked:
         st.session_state.eng = eng
         st.session_state.last_company = company  # Aktualisiere den zuletzt gescrapten Firmennamen
 
+        # Generierung der Visualisierungen
+        df_list = [st.session_state.df[st.session_state.df['company'] == key] for key in st.session_state.df['company'].unique()]
+        st.session_state.histlist = [create_histplot(x) for x in df_list]
+        st.session_state.linelist = [create_lineplot(x) for x in df_list]
+        st.session_state.word_cloud_list = [create_wordcloud(x, st.session_state.eng, company) for x, company in zip(df_list, st.session_state.df['company'].unique())]
+        st.session_state.visuals_generated = True
+
     else:
         st.error('Unbekannte Website.')
 
     st.session_state.scrape_button_clicked = False  # Setze den Button-Status zurück
 
-df = st.session_state.get('df', pd.DataFrame())
-
 # Überprüfen, ob der DataFrame gültig ist und die Spalte existiert
+df = st.session_state.get('df', pd.DataFrame())
 if not df.empty and 'company' in df.columns:
-    df_list = [df[df['company'] == key] for key in df['company'].unique()]
-    histlist = [create_histplot(x) for x in df_list]
-    linelist = [create_lineplot(x) for x in df_list]
-    word_cloud_list = [create_wordcloud(x, eng, company) for x in df_list]
     col1, col2 = st.columns([5, 2])
 
     with col1:
-        for e in range(len(st.session_state.company)):
-            st.markdown(f'#### Bewertungsübersicht für {st.session_state.company[e]}:')
-            st.pyplot(histlist[e])
-            st.pyplot(linelist[e])
-            st.image(word_cloud_list[e], use_column_width=True)
+        if 'visuals_generated' in st.session_state:
+            for e in range(len(st.session_state.company)):
+                st.markdown(f'#### Bewertungsübersicht für {st.session_state.company[e]}:')
+                st.pyplot(st.session_state.histlist[e])
+                st.pyplot(st.session_state.linelist[e])
+                st.image(st.session_state.word_cloud_list[e], use_column_width=True)
+        else:
+            st.markdown('Keine Daten vorhanden oder Visualisierungen konnten nicht geladen werden.')
 
     with col2:
         st.markdown('#### Dataframe')
@@ -91,7 +94,7 @@ if not df.empty and 'company' in df.columns:
             st.session_state.run_model_clicked = False
 
         def click_run_model():
-            st.session_state.run_model_clicked = not st.session_state.run_model_clicked
+            st.session_state.run_model_clicked = True
 
         run_model = st.button('Model trainieren', on_click=click_run_model)
 
@@ -102,7 +105,7 @@ if not df.empty and 'company' in df.columns:
                 if len(df) < 100:
                     st.warning('Sehr geringe Datenmenge - Ergebnisse können ungenau sein.')
 
-                # Train model if not already done
+                # Train model if not already done or if new data has been scraped
                 if 'model' not in st.session_state or 'vect' not in st.session_state or st.session_state.df is not df:
                     X_transformed, y, vect = preprocess_data(df)
                     model, rmse = get_best_model(X_transformed, y)
@@ -116,9 +119,9 @@ if not df.empty and 'company' in df.columns:
                     vect = st.session_state.vect
 
                 st.markdown(f"Mittlere Abweichung des Models: {rmse:.2f} Sterne von der eigentlichen Bewertung")
-            
+
                 comment = st.text_input('Zu prüfenden Kommentar eingeben:')
-            
+
                 if comment:
                     try:
                         comment_tf = vect.transform([comment])
